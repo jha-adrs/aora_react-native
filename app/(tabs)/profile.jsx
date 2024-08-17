b898477f205fa44a9d25be53c3108c205167a2a8
Aadarsh Jha <aadarshjha6783@gmail.com>
2024-08-17 10:59:04 +0000

    Basic setup and assets

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="text-white font-semibold">Profile</Text>
+    </View>
+  )
+}
+
+export default Profile
+
+const styles = StyleSheet.create({})
